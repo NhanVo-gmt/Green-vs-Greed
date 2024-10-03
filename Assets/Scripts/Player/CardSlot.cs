8c2fcdeaa0b98bcf9ca3fd806fa31de5eca52b07
NhanVoo <thanhnhan@osogames.studio>
2024-10-03 21:03:39 +1000

    adding card

---
+++ Assets/Scripts/Player/CardSlot.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSlot : MonoBehaviour
+{
+    public Card card;
+
+    public void AddCard(Card card)
+    {
+        this.card = card;
+    }
+
+    public void UseCard()
+    {
+        
+    }
+}
