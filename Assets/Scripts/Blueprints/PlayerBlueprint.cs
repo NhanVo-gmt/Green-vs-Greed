a9617099adb9200496158b5da4692f2aa242ae91
NhanVoo <thanhnhan@osogames.studio>
2024-10-27 18:30:42 +1100

    adding level to game

--- Assets/Scripts/Blueprints/PlayerBlueprint.cs
+++ Assets/Scripts/Blueprints/PlayerBlueprint.cs
@@ -14,6 +14,20 @@ namespace Blueprints
         public int                                      Id;
         public PlayerType                               PlayerType;
         public BlueprintByRow<Resource, ResourceRecord> Resources;
+        public BlueprintByRow<PlayerUpgrade>            PlayerUpgrades;
     }
 
+    [CsvHeaderKey("Level")]
+    public class PlayerUpgrade
+    {
+        public int                                      Level;
+        public BlueprintByRow<Resource, PlayerUpgradeRequirement> Requirements;
+    }
+
+    [CsvHeaderKey("ResourceRequirement")]
+    public class PlayerUpgradeRequirement
+    {
+        public Resource ResourceRequirement;
+        public int      ResourceRequirementValue;
+    }
 }
